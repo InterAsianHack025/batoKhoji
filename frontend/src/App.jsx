d33600be62d0712b880f412f1a6f2694d041f454
Rinju Pokhrel1 <rinjupokhrel91@gmail.com>
2025-09-05 09:09:51 +0545

    header setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
-export default function App() {
-  return (
-    <div>
-      <h1 className="text-4xl text-red-500 font-bold underline">
-        Hello Tailwind!
-      </h1>
-    </div>
-  )
-}
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import RoutePage from "./components/Route";
+import RoutePlanner from "./components/RoutePlanner";
+import CalendarReminder from "./components/CalendarReminder";
+import SavedPlace from "./components/SavedPlace";
+import ChatWidget from "./components/ChatWidget";
+import LiveBusTracking from "./components/LiveBusTracking";
+import RecentTrips from "./components/RecentTrips";
