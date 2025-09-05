b4cc6b2589c99d78c60b8f5f58fe5bd72b475bf2
Harina Khati <harina.khati@gmail.com>
2025-09-05 09:17:42 +0545

    merge conflict resolved

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,7 @@
 import { defineConfig } from 'vite'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-=======
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
->>>>>>> 1a74a76a073730df3529fdcc98dfd12619d1f0de
 })
